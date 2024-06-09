f806d7d7dd819bf5fd994d245c4f5d9de0c546ea
truongnn1802 <truongnn2081@gmail.com>
2024-06-09 21:44:24 +0700

    code dark_mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './them.js';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import theme from './theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    {/* Dùng Css CssVarsProvider thay cho ThemeProvider: Tránh trang bị nhấp nháy khi dùng Server side rendering*/}
+    <CssVarsProvider theme={theme}> 
     <CssBaseline />
     <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </React.StrictMode>,
 )
